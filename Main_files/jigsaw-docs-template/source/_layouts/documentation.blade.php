40839ae7a1cd545d9ecce5255c14814bd3aa8f8f
marco76tv <marco.sottana@gmail.com>
2020-08-26 22:01:43 +0200

    .

--- Main_files/jigsaw-docs-template/source/_layouts/documentation.blade.php
+++ Main_files/jigsaw-docs-template/source/_layouts/documentation.blade.php
@@ -1,18 +1,18 @@
-@extends('pub_theme::layouts.app')
+@extends('_layouts.master')
 
 @section('nav-toggle')
-    @include('pub_theme::layouts.nav.menu-toggle')
+    @include('_nav.menu-toggle')
 @endsection
 
-@section('content')
+@section('body')
 <section class="container max-w-8xl mx-auto px-6 md:px-8 py-4">
     <div class="flex flex-col lg:flex-row">
         <nav id="js-nav-menu" class="nav-menu hidden lg:block">
-            @include('pub_theme::layouts.nav.menu', ['items' => $page->navigation ])
+            @include('_nav.menu', ['items' => $page->navigation])
         </nav>
 
         <div class="DocSearch-content w-full lg:w-3/5 break-words pb-16 lg:pl-4" v-pre>
-            {!! $row->txt !!}
+            @yield('content')
         </div>
     </div>
 </section>
